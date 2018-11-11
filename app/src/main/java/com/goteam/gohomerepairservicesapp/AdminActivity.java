5276cee75808b1e53dac38791d1dce92773968a0
Cedric <ced2142b@gmail.com>
2018-11-10 23:28:01 -0500

    Finished service functionality

--- app/src/main/java/com/goteam/gohomerepairservicesapp/AdminActivity.java
+++ app/src/main/java/com/goteam/gohomerepairservicesapp/AdminActivity.java
@@ -14,13 +14,16 @@ import android.util.Patterns;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
@@ -29,9 +32,11 @@ import java.util.LinkedList;
 public class AdminActivity extends AppCompatActivity {
 
     private RecyclerView recyclerView;
-    private RecyclerView.Adapter adapter;
+    private ServiceAdapter adapter;
     private RecyclerView.LayoutManager layoutManager;
     private LinkedList<Service> userList;
+    private TextView userEmail, userName, userAccountType;
+    private FirebaseUser firebaseUser;
 
     final FirebaseDatabase database = FirebaseDatabase.getInstance();
     String TAG= "AdminActivity";
@@ -40,13 +45,23 @@ public class AdminActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin);
+        userEmail = findViewById(R.id.emailView);
+        userName = findViewById(R.id.nameView);
+        userAccountType = findViewById(R.id.accountTypeView);
+        loadUserInformation();
         recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
         recyclerView.setHasFixedSize(true);
         layoutManager = new LinearLayoutManager(this);
+        userList=new LinkedList<Service>();
         recyclerView.setLayoutManager(layoutManager);
         adapter= new ServiceAdapter(getSerivces());
-        userList=new LinkedList<Service>();
+        recyclerView.setAdapter(adapter);
+        setUpList();
+
     }
+
+
+
     public void btnLogoutClicked(View view) {
         FirebaseAuth.getInstance().signOut();
         finish();
@@ -68,7 +83,7 @@ public class AdminActivity extends AppCompatActivity {
 
 
     private void addService(String name, double rate){
-        Service service=new Service(name,rate);
+        final Service service=new Service(name,rate);
         if(serviceSimpleVerification(name,rate)){
             database.getReference("Services").child(service.getServiceName()).setValue(service).addOnCompleteListener(new OnCompleteListener<Void>() {
                 @Override
@@ -78,7 +93,8 @@ public class AdminActivity extends AppCompatActivity {
                         Toast.makeText(AdminActivity.this, "Failed to create new service. Please try again.", Toast.LENGTH_LONG).show();
                         return;
                     }
-
+                    userList.add(service);
+                    adapter.notifyDataSetChanged();
                     Toast.makeText(AdminActivity.this, "You have successfully created a new service.", Toast.LENGTH_LONG).show();
                 }
             });
@@ -102,7 +118,6 @@ public class AdminActivity extends AppCompatActivity {
                 EditText rateInput=dialog.findViewById(R.id.rateRegister);
                 String serviceName=serviceInput.getText().toString();
                 Double rate=Double.valueOf(rateInput.getText().toString());
-                System.out.print("test");
 
                 addService(serviceName,rate);
             }
@@ -115,6 +130,7 @@ public class AdminActivity extends AppCompatActivity {
             }
         });
         inputDialog.show();
+
 
     }
 
@@ -137,5 +153,105 @@ public class AdminActivity extends AppCompatActivity {
 
         return userList;
 
+    }
+
+   private void setUpList(){
+
+        adapter.setOnCardClick(new ServiceAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(final int position) {
+                AlertDialog.Builder inputDialog= new AlertDialog.Builder(AdminActivity.this);
+
+
+                inputDialog.setTitle("Edit Rate");
+
+                inputDialog.setView(R.layout.editrate);
+
+                inputDialog.setPositiveButton("Done", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Dialog dialog= (Dialog) dialogInterface;
+                        EditText rateInput=dialog.findViewById(R.id.rateEdit);
+                        Double rate=Double.valueOf(rateInput.getText().toString());
+                        userList.get(position).setRate(rate);
+                        database.getReference("Services").child(userList.get(position).getServiceName()).child("rate").setValue(rate);
+                        adapter.notifyDataSetChanged();
+
+                    }
+                });
+
+                inputDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.cancel();
+                    }
+                });
+                inputDialog.show();
+
+            }
+
+            @Override
+            public void onDeleteClick(int position) {
+                database.getReference("Services").child(userList.get(position).getServiceName()).removeValue();
+                userList.remove(position);
+                adapter.notifyItemRemoved(position);
+            }
+        });
+    }
+
+
+    private void loadUserInformation() {
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        if (firebaseUser == null) {
+            finish();
+            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+        }
+
+        userEmail.setText("Email address: " + firebaseUser.getEmail());
+        userName.setText("Name: " + firebaseUser.getDisplayName());
+
+        DatabaseReference userInfoReference = FirebaseDatabase.getInstance().getReference("Users").child(firebaseUser.getUid());
+
+        userInfoReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                User user = null;
+                String role = (String) dataSnapshot.child("role").getValue();
+
+                if (role == null) {
+                    Log.e(TAG, "Failed to fetch user info: role entry is not set");
+                    Toast.makeText(AdminActivity.this, "Failed to fetch user info", Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                switch (role) {
+                    case "admin":
+                        user = dataSnapshot.getValue(Admin.class);
+                        break;
+
+                    case "homeowner":
+                        user = dataSnapshot.getValue(Homeowner.class);
+                        break;
+
+                    case "provider":
+                        user = dataSnapshot.getValue(ServiceProvider.class);
+                        break;
+                }
+
+                if (user == null) {
+                    Log.e(TAG, "Failed to fetch user info: could not create user object");
+                    Toast.makeText(AdminActivity.this, "Failed to fetch user info", Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                userAccountType.setText("Role: " + user.getRoleName());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 }
