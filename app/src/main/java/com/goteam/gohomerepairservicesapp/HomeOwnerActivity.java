1a656a3d0f710fbb634ae9562bf6b3799e16f414
Nicolas Gnyra <nicolas@gnyra.com>
2018-12-04 00:13:30 -0500

    Use spinner for services

--- app/src/main/java/com/goteam/gohomerepairservicesapp/HomeOwnerActivity.java
+++ app/src/main/java/com/goteam/gohomerepairservicesapp/HomeOwnerActivity.java
@@ -7,45 +7,45 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
-import android.view.KeyEvent;
 import android.view.View;
-import android.view.inputmethod.EditorInfo;
-import android.widget.EditText;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.RadioGroup;
-import android.widget.TextView;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 
 public class HomeOwnerActivity extends AppCompatActivity {
-
     private static final String TAG = "HomeownerActivity";
-    EditText searchText;
     LinkedList<ServiceProvider> serviceProviders;
     LinkedList<ServiceProvider> resultServiceProviders;
+    LinkedList<String> services;
     final FirebaseDatabase database = FirebaseDatabase.getInstance();
-    DatabaseReference reference = database.getReference("Users");
-    private FirebaseAuth firebaseAuth;
     private RadioGroup searchType;
     private RecyclerView providerRecycler;
     private HO_SPAdapter spRecyclerAdapter;
+    private Spinner servicesSpinner;
+    private ArrayAdapter<String> servicesAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         serviceProviders = new LinkedList<>();
         resultServiceProviders = new LinkedList<>();
+        services = new LinkedList<>();
         setContentView(R.layout.activity_home_owner);
-        searchText = findViewById(R.id.search_input);
+        //searchText = findViewById(R.id.search_input);
         searchType = findViewById(R.id.searchType);
         providerRecycler = findViewById(R.id.provider_recycler);
+        servicesSpinner = findViewById(R.id.servicesSpinner);
 
         //Setting up recyclerView
         spRecyclerAdapter = new HO_SPAdapter(resultServiceProviders);
@@ -53,22 +53,35 @@ public class HomeOwnerActivity extends AppCompatActivity {
         providerRecycler.setLayoutManager(new LinearLayoutManager(this));
         providerRecycler.setAdapter(spRecyclerAdapter);
 
+        servicesAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, services);
+        servicesAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        servicesSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                resultServiceProviders.clear();
+                String item = servicesAdapter.getItem(i);
+
+                if (i > 0 && item != null)
+                    for (ServiceProvider provider : serviceProviders)
+                        for (Service service : provider.getServices())
+                            if (service.getServiceName() != null && service.getServiceName().toLowerCase().contains(item.toLowerCase()))
+                                resultServiceProviders.add(provider);
+
+                spRecyclerAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                resultServiceProviders.clear();
+            }
+        });
+        servicesSpinner.setAdapter(servicesAdapter);
+
         setListeners();
         loadServiceProviders();
     }
 
     private void setListeners() {
-        searchText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-            @Override
-            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                if (actionId == EditorInfo.IME_ACTION_SEARCH) {
-                    searchProvider(v.getText().toString());
-                    return true;
-                }
-                return false;
-            }
-        });
-
         spRecyclerAdapter.setOnCardClick(new HO_SPAdapter.OnItemClickListener() {
             @Override
             public void onItemClick(int position) {
@@ -79,7 +92,29 @@ public class HomeOwnerActivity extends AppCompatActivity {
     }
 
     private void loadServiceProviders() {
-        reference.addListenerForSingleValueEvent(new ValueEventListener() {
+        database.getReference("Services").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                servicesAdapter.add("Select a service...");
+
+                for (DataSnapshot ds : dataSnapshot.getChildren()) {
+                    Service service = ds.getValue(Service.class);
+                    String name = service.getServiceName();
+
+                    if (name != null)
+                        services.add(name);
+                }
+
+                servicesAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        database.getReference("Users").addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 for (DataSnapshot user : dataSnapshot.getChildren()) {
@@ -108,7 +143,6 @@ public class HomeOwnerActivity extends AppCompatActivity {
         int selectedButton = searchType.getCheckedRadioButtonId();
 
         switch (selectedButton) {
-
             case (R.id.radioService):
                 for (ServiceProvider provider : serviceProviders) {
                     for (Service service : provider.getServices()) {
@@ -120,7 +154,6 @@ public class HomeOwnerActivity extends AppCompatActivity {
 
                 break;
 
-
             case (R.id.radioRating):
                 if (!text.matches("[0-5].?[0-9]?")) {
                     Toast.makeText(HomeOwnerActivity.this, "Please enter a rating between 0.0 and 5.0", Toast.LENGTH_LONG).show();
@@ -128,11 +161,10 @@ public class HomeOwnerActivity extends AppCompatActivity {
                 }
 
                 for (ServiceProvider provider : serviceProviders)
-                    if (provider.getRating() == Double.valueOf(text))
+                    if (provider.getRating() >= Double.valueOf(text))
                         resultServiceProviders.add(provider);
 
                 break;
-
 
             case (R.id.radioTime):
                 //TO IMPLEMENT BY NIC
